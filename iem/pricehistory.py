1772cac82fd8c4a29b3c8fa01c0870088936a134
Reece Heineke <rheineke@users.noreply.github.com>
2016-06-05 19:13:39 -0400

    Full price history

--- iem/pricehistory.py
+++ iem/pricehistory.py
@@ -1,12 +1,24 @@
+import datetime as dt
+import itertools
+
 import pandas as pd
 import requests
 
 import iem
 
 
+def full_price_history_frame(mkt_id):
+    month_year_iter = history_dates(mkt_id=mkt_id)
+    dfs = []
+    for m, y in month_year_iter:
+        # TODO: Handle future combinations
+        dfs.append(price_history_frame(mkt_id, m, y))
+    return pd.concat(dfs)
+
+
 def price_history_frame(mkt_id, year, month):
     """Returns price history as a DataFrame"""
-    url = iem.LEGACY_URL + '/pricehistory/PriceHistory_GetData.cfm'
+    url = iem.LEGACY_URL + 'pricehistory/PriceHistory_GetData.cfm'
     data = dict(Market_ID=mkt_id, Month='{:02d}'.format(month), Year=year)
     response = requests.post(url=url, data=data)
     index_cols = [iem.DATE, iem.CONTRACT]
@@ -18,9 +30,29 @@ def price_history_frame(mkt_id, year, month):
 
     return dfs[0]
 
+
+def history_dates(mkt_id):
+    url = iem.LEGACY_URL + 'pricehistory/pricehistory_selectcontract.cfm'
+    response = requests.get(url=url, params={'Market_ID': mkt_id})
+    dfs = pd.read_html(response.text, index_col=0)
+
+    # Expect a singleton list
+    assert len(dfs) == 1
+
+    df = dfs[0]
+
+    mon_str = df.ix['Month:'][1]
+    months = [dt.datetime.strptime(s[:3], '%b').month for s in mon_str.split()]
+    year_str = df.ix['Year'][1]
+    years = [int(s) for s in year_str.split()]
+
+    return itertools.product(years, months)
+
 if __name__ == '__main__':
     mkt_id = iem.Market.RCONV16.value
-    year = 2016
-    month = 6
-    px_hist_df = price_history_frame(mkt_id, year, month)
+    # year = 2016
+    # month = 6
+    # px_hist_df = price_history_frame(mkt_id, year, month)
+    px_hist_dfs = full_price_history_frame(mkt_id)
+
 
