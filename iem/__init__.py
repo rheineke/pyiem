fc88eddb0b6cf8250840bade0493d20053b7f413
Reece Heineke <rheineke@users.noreply.github.com>
2016-06-12 20:55:50 -0400

    Ready for next rate decision

--- iem/__init__.py
+++ iem/__init__.py
@@ -36,44 +36,6 @@ BID = 'bid'
 
 
 @unique
-class Market(Enum):
-    """
-    IEM numeric designation for each market. A market contains a set of
-    arbitrageable contracts
-    """
-    FedPolicyB = 51
-    Congress16 = 360
-    PRES16_VS = 361
-    PRES16_WTA = 362
-    RCONV16 = 364
-    # DCONV16 = 365
-
-
-@unique
-class Congress16(Enum):
-    DH_DS16 = 3014
-    RH_DS16 = 3015
-    DH_RS16 = 3016
-    RH_RS16 = 3017
-    OTHER16 = 3018
-
-
-@unique
-class RConv16(Enum):
-    CARS_NOM = 3034
-    CRUZ_NOM = 3035
-    RUBI_NOM = 3036
-    TRUM_NOM = 3037
-    RROF_NOM = 3038
-
-
-def market_contract_assets(market):
-    if type(market) == str:
-        mkt_assets = {
-            Market.Congress16.name: Congress16,
-        }
-    else:
-        mkt_assets = {
-            Market.Congress16: Congress16,
-        }
-    return mkt_assets[market]
+class Side(Enum):
+    BUY = 0
+    SELL = 1
