f109ef825c276e1e25f34470d2800606b9f73506
Reece Heineke <rheineke@users.noreply.github.com>
2016-09-06 22:11:20 -0400

    Price history

--- analysis/hdf_store.py
+++ analysis/hdf_store.py
@@ -10,10 +10,22 @@ def open_store(mode=None):
 
 if __name__ == '__main__':
     mkt_name = 'FedPolicyB'
-    print(mkt_name)
-    mkt = iem.Market(mkt_name)
-    mkt_id = mkt.value
-    px_hist_dfs = px_hist._full_price_history_frames(mkt_id)
+    # print(mkt_name)
+    # mkt = iem.Market(mkt_name)
+    # mkt_id = mkt.value
+    # px_hist_dfs = px_hist._full_price_history_frames(mkt_id)
 
-    # with open_store() as hdf_store:
-    #     hdf_store[mkt_name] = px_hist_df
+    # FedPolicyB data cleaning
+    # 2001/10 data has a warning about the contract type
+    # df = px_hist_dfs[9]
+    # tail_df = df.iloc[1:]
+    # dt_idx = pd.to_datetime(tail_df.index.get_level_values(level=iem.DATE))
+    # contract_idx = tail_df.index.get_level_values(level=iem.CONTRACT)
+    # levels = [dt_idx, contract_idx]
+    # multi_idx = pd.MultiIndex(levels=levels, labels=tail_df.index.labels)
+    # clean_df = tail_df.set_index(multi_idx)
+    # px_hist_dfs[9] = clean_df
+    # px_hist_df = pd.concat(px_hist_dfs)
+
+    with open_store(mode='r') as hdf_store:
+        px_hist_df = hdf_store[mkt_name]
