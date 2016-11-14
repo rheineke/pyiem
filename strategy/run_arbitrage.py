345a5e4f69886fadbe902bf2c0ec1d4e00522090
Reece Heineke <rheineke@users.noreply.github.com>
2016-11-14 12:33:47 -0300

    Minor

--- strategy/run_arbitrage.py
+++ strategy/run_arbitrage.py
@@ -25,11 +25,11 @@ def arbitrage_orders(direction, quantity=1):
     # TODO: Most efficient action is to determine if single buy is lower price
     # than bundle + 2 sales
     expiration = '2016/06/30 11:59 PM'
-    dir_pt_lmt = PriceTimeLimit(.20, expiration)
+    dir_pt_lmt = PriceTimeLimit(.200, expiration)
     dir_contract = Contract('FRsame0616')
     contract_bundle = ContractBundle('FedPolicyB', '0616')
     other_contracts = [Contract('FRup0616'), Contract('FRdown0616')]
-    other_pt_lmt = PriceTimeLimit(0.90, expiration)
+    other_pt_lmt = PriceTimeLimit(0.900, expiration)
     return [
         # Buy direction asset
         Single(dir_contract, Side.BUY, quantity, dir_pt_lmt),
