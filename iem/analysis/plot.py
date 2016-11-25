0bc828bbd6a9914a538e952e8469d28f6ed44174
Reece Heineke <reece.heineke@gmail.com>
2016-11-25 16:01:59 -0500

    ENH: Cumulative volume by date (#4)

--- iem/analysis/plot.py
+++ iem/analysis/plot.py
@@ -3,11 +3,10 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 import iem
-from iem import config
-from iem.analysis import hdf_store
+from iem import config, pricehistory as px_hist, recorder
+from iem import contract
 
 EXPIRY = 'expiry'
-EXPIRY_DATE = 'expiry_date'
 DAYS_TO_EXPIRY_DATE = 'days_to_expiry_date'
 
 
@@ -24,43 +23,77 @@ def expiry(contract_name):
 
 
 def expiry_date_series(expiry_str, *, expiry_date_json):
-    return expiry_date_json.get(expiry_str, pd.NaT)
+    bundle = expiry_date_json.get(expiry_str, {})
+    return bundle.get(config.EXPIRY_DATE, pd.NaT)
 
+
+def cumulative_units_frame(bundle_history_df):
+    units_df = bundle_history_df[iem.UNITS].unstack()
+    return units_df.cumsum()
+
+
+def bundle_slice(json_obj, market_name, expiry_date_str):
+    bundle_dict = config.find_bundle(json_obj, market_name, expiry_date_str)
+    bundle_assets = list(bundle_dict[config.ASSETS].keys())
+    return pd.IndexSlice[:, bundle_assets]
+
+
+def plot_cumulative_units(bundle_history_df):
+    cum_units_df = cumulative_units_frame(bundle_history_df)
+    # First trade index
+    fst_trade_idx = (cum_units_df.sum(axis=1) > 0).idxmax()
+    nonzero_cum_units_df = cum_units_df.loc[fst_trade_idx:]
+    plt.figure(get_new_fignum())
+    plt.plot(nonzero_cum_units_df)
+    plt.show()
 
 if __name__ == '__main__':
     mkt_name = 'FedPolicyB'
-    with hdf_store.open_store(mode='r') as hdf_store:
-        px_hist_df = hdf_store[mkt_name]
+    mkt = contract.Market(mkt_name)
+    with recorder.open_store(mode='r') as hdf_store:
+        px_hist_df = hdf_store[recorder.history_key(market=mkt)]
 
-    mkts_conf = config.read_markets()
+    mkts_json = config.read_markets()
+    mkt_conf = mkts_json[mkt_name]
 
     # Clean dataframe
     df = px_hist_df.copy().reset_index()
     # Expiry
     df[EXPIRY] = df[iem.CONTRACT].apply(expiry)
     # Expiry date
-    kwargs = {'expiry_date_json': mkts_conf[mkt_name]['expiry_date']}
-    expiry_dt_srs = df[EXPIRY].apply(expiry_date_series, **kwargs)
-    df[EXPIRY_DATE] = expiry_dt_srs
+    expiry_date_json = mkt_conf[config.BUNDLE]
+    kwargs = {'expiry_date_json': expiry_date_json}
+    df[config.EXPIRY_DATE] = df[EXPIRY].apply(expiry_date_series, **kwargs)
     # Days to expiration
-    df[DAYS_TO_EXPIRY_DATE] = df[EXPIRY_DATE] - df[iem.DATE]
-    # sept16_lbl = df[iem.CONTRACT].str.contains('0916')
-    gb = df.groupby([EXPIRY, iem.DATE])
-    agg_dict = {
-        iem.UNITS: np.sum,
-        iem.DVOL: np.sum,
-        iem.LOW_PX: np.sum,
-        iem.HIGH_PX: np.sum,
-        iem.AVG_PX: np.sum,
-        iem.LST_PX: np.sum,
-        EXPIRY_DATE: 'first',
-        DAYS_TO_EXPIRY_DATE: 'first',
-    }
-    agg_df = gb.agg(agg_dict)
-    # Clean aggregate
-    agg_df[EXPIRY_DATE] = pd.to_datetime(agg_df[EXPIRY_DATE])
-    agg_df[DAYS_TO_EXPIRY_DATE] = pd.to_timedelta(agg_df[DAYS_TO_EXPIRY_DATE])
-    sept16_df = agg_df.loc['0916']
+    df[DAYS_TO_EXPIRY_DATE] = df[config.EXPIRY_DATE] - df[iem.DATE]
+
+    # Bundle frame
+    bundle = '1116'
+    bundle_assets = list(mkt_conf[config.BUNDLE][bundle][config.ASSETS].keys())
+    bundle_lbl = df[iem.CONTRACT].str.contains(bundle)
+    bundle_df = df.loc[bundle_lbl]
+
+    bundle_df = px_hist_df.loc[bundle_slice(mkts_json, mkt_name, bundle), :]
+    plot_cumulative_units(bundle_df)
+
+    plot_cumulative_units(px_hist_df)
+
+    # gb = df.groupby([EXPIRY, iem.DATE])
+    # agg_dict = {
+    #     iem.UNITS: np.sum,
+    #     iem.DVOL: np.sum,
+    #     iem.LOW_PX: np.sum,
+    #     iem.HIGH_PX: np.sum,
+    #     iem.AVG_PX: np.sum,
+    #     iem.LST_PX: np.sum,
+    #     config.EXPIRY_DATE: 'first',
+    #     DAYS_TO_EXPIRY_DATE: 'first',
+    # }
+    # agg_df = gb.agg(agg_dict)
+    # # Clean aggregate
+    # agg_df[config.EXPIRY_DATE] = pd.to_datetime(agg_df[config.EXPIRY_DATE])
+    # agg_df[DAYS_TO_EXPIRY_DATE] = pd.to_timedelta(agg_df[DAYS_TO_EXPIRY_DATE])
+    # bundle_df = agg_df.loc[bundle]
     # plt.figure(get_new_fignum())
     # TODO: Match price history graphs on website
     # TODO: Construct days to expiration. Useful feature?
