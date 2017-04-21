71d4a69ee0deac00760b01f7c71a30f5cd958817
Reece Heineke <reece.heineke@gmail.com>
2017-04-20 21:16:33 -0400

    ENH: Tables created (#9)

--- run_mysql_market_data_recorder.py
+++ run_mysql_market_data_recorder.py
@@ -2,11 +2,26 @@ import json
 
 import sqlalchemy as sa
 
+from iem import config
+
 if __name__ == '__main__':
-    engine_fmt = '{dialect}{driver}://{user}:{password}@{host}/{dbname}'
+    base_engine_fmt = '{dialect}+{driver}://{user}:{password}@{host}'
     db_json_fp = 'conf/db.json'
     with open(db_json_fp, 'r') as fp:
         db_conf = json.load(fp)
 
-    engine_arg = engine_fmt.format(**db_conf)
-    engine = sa.create_engine(engine_arg)
+    base_engine = sa.create_engine(base_engine_fmt.format(**db_conf))
+    create_fmt = 'CREATE DATABASE IF NOT EXISTS {dbname}'
+    base_engine.execute(create_fmt.format(**db_conf))
+
+    engine_fmt = base_engine_fmt + '/{dbname}'
+    engine = sa.create_engine(engine_fmt.format(**db_conf))
+
+    metadata = sa.MetaData()
+    mkt_table = config.markets_table(metadata)
+    bundles_table = config.bundles_table(metadata)
+    assets_table = config.assets_table(metadata)
+    metadata.create_all(engine)
+
+    mkt_conf = config.read_markets()
+    # TODO: Transform into Pandas dataframes and insert?
