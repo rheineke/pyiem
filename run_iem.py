533fbe86a877ce0d157d8efd97f53d1112912377
Reece Heineke <reece.heineke@gmail.com>
2017-05-10 07:04:00 -0400

    Updated requirements

--- run_iem.py
+++ run_iem.py
@@ -7,20 +7,26 @@ if __name__ == '__main__':
     login_kwargs = read_login()
     sess = Session(**login_kwargs)
     login_response = sess.authenticate()
+
     # Get Orderbook dataframe
     mkt = Market('FedPolicyB')
     ob_df = sess.orderbook(mkt)
+
     # Get order activity
     asset = Contract(mkt.name, 'FRup1216')
     print(asset)
     oa_df = sess.holdings(asset)
+
     # Get outstanding orders
     oo_df = sess.outstanding_orders(asset, iem.BID)
+
     # Get trade messages
     msg_df = sess.messages(mkt)
+
     # Send order
     # contract = 3037  # RCONV16.TRUMP_NOM
     # price_time_limit = PriceTimeLimit(.25, '20161102')
     # o = Single(contract, Side.BUY, 1, price_time_limit)
     # latest_ob_df = sess.place_order(o)
+
     logout_response = sess.logout()
